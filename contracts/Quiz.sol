0c573c6bb51208e5f01443761f1db99f6a53ec29
h44rd <panchalhaard@gmail.com>
2018-11-02 01:02:30 +0530

    Added more code

--- contracts/Quiz.sol
+++ contracts/Quiz.sol
@@ -73,8 +73,6 @@ contract Quiz{
         _;
     }
 
-
-
     struct Player {
         uint p_fee;
         uint reward;
@@ -96,6 +94,113 @@ contract Quiz{
 
     struct QuizMaster {
         address add;
+    }
+
+    mapping (uint => Question) question_details;
+    mapping (address => Player) add_to_player;
+
+    function register_quiz_master () public
+    quizmasterDoesNotExist
+    returns(bool) {
+        quiz_master = msg.sender;
+    }
+
+    function add_question (string _ques, uint _ans) public
+    onlyQuizMaster
+    returns (bool) {
+        require(registration_started == false, "Participants registration started");
+        require(_ans > 0 && _ans < 5, "Invalid answer");
+        question_details[question_count].text = _ques;
+        question_details[question_count].correct_ans = _ans;
+        question_count = question_count + 1;
+    }
+
+    function start_registration (uint _N, uint _st_off, uint _et_off) public
+    onlyQuizMaster
+    returns(bool) {
+        require(registration_started == false, "Registration already started");
+        require(question_count > 0, "No questions");
+
+        N = _N;
+        starttime = block.number + _st_off;
+        endtime = starttime + _et_off;
+
+        uint i;
+        for(i = 0; i < question_count; i++) {
+            question_details[i].starttime = starttime + i*(endtime - starttime)/question_count;
+            question_details[i].endtime = starttime + (i+1)*(endtime - starttime)/question_count;
+        }
+
+        registration_started = true;
+
+        return true;
+    }
+
+    function register_player() public onlyBefore(starttime) returns(bool) {
+        require(registration_started == true, "Registration has not started yet");
+        require(msg.sender != quiz_master, "You are not allowed, good sir.");
+        require(player_count < N, "Number of players exceed the max. count of players allowed");
+        require(!add_to_player[msg.sender].paid && !add_to_player[msg.sender].pending, "Pending or paid already");
+
+        Player storage p = add_to_player[msg.sender];
+        p.pending = true;
+        p.paid = false;
+        p.reward = 0;
+        p.answer = 0;
+        p.p_fee = calc_entry_fee();
+        return true;
+    }
+
+    function calc_entry_fee() public returns(uint) {
+        return 16e18;
+    }
+
+    function get_entry_fee() public view isRegistered(msg.sender) returns(uint) {
+        // require(add_to_player[msg.sender].pending == true || add_to_player[msg.sender].paid == true, "Not registered");
+        require(registration_started == true, "Registration has not started.");
+        require(msg.sender != quiz_master, "You are the quiz master.");
+        return add_to_player[msg.sender].p_fee;
+        // return block.number;
+    }
+
+    function pay_reg_fee() payable public onlyBefore(starttime) returns(bool) {
+        require(add_to_player[msg.sender].pending == true, "Not pending");
+        require(add_to_player[msg.sender].paid == false, "Fee already paid!");
+        require(msg.value == add_to_player[msg.sender].p_fee, "Amount paid does not match entry fee");
+        require(msg.sender != quiz_master, "You are the quiz master.");
+
+
+        Player storage p = add_to_player[msg.sender];
+        p.paid = true;
+        p.pending = false;
+
+        player_count = player_count + 1;
+        total_pfee = total_pfee + msg.value;
+
+        return true;
+    }
+
+    function get_quiz_start_time() public view isRegistered(msg.sender) returns(uint) {
+        return starttime;
+    }
+
+
+    function get_quiz_end_time() public view isRegistered(msg.sender) returns(uint) {
+        return endtime;
+    }
+
+    function get_question() public
+    onlyAfter(starttime)
+    onlyBefore(endtime)
+    hasPaid(msg.sender) view returns(string) {
+        // require(add_to_player[msg.sender].paid == true, "You haven't paid or haven't registered.");
+        uint i;
+        for(i = 0; i < question_count; i++) {
+            if(question_details[i].endtime > block.number && question_details[i].starttime <= block.number) {
+                // question_details[i].if_viewed[msg.sender] = true;
+                return question_details[i].text;
+            }
+        }
     }
 
 }
--- test/qm.js
+++ test/qm.js
@@ -2,12 +2,12 @@ var expectThrow = require('./helper.js');
 const Quiz = artifacts.require("Quiz");
 
 contract("Quiz", async(accounts) => {
-    var quiz;  
+    var quiz;
 
     it("tests that two people can't register as quiz master", async () => {
         quiz = await Quiz.new({from: accounts[0]});
         let res1 = await quiz.register_quiz_master({from: accounts[0]});
-        let res2 = quiz.register_quiz_master({from: accounts[1]}); 
+        let res2 = quiz.register_quiz_master({from: accounts[1]});
         await expectThrow(res2);
     })
 
@@ -35,6 +35,42 @@ contract("Quiz", async(accounts) => {
         let res2 = await quiz.register_player({from: accounts[1]});
         let res3 = quiz.add_question("Which is an even prime? - 1. 1 2. 2 3. 3 4. 4", 2, {from: accounts[0]});
         await expectThrow(res3);
+    })
+
+    it("tests that player can't pay arbitrary fee", async () => {
+        let res1 = quiz.pay_reg_fee({from: accounts[1], value: 10e18});
+        await expectThrow(res1);
+    })
+
+    it("tests that player can pay fee", async () => {
+        let res1 = await quiz.pay_reg_fee({from: accounts[1], value: 16e18});
+    })
+
+    it("tests that quizmaster can't participate in quiz", async () => {
+        let res1 = quiz.register_player({from: accounts[0]});
+        await expectThrow(res1);
+    })
+
+    it("tests that players doesn't get question before quiz starts", async () => {
+        let res1 = quiz.get_question({from: accounts[1]});
+        await expectThrow(res1);
+    })
+
+    it("tests that players get question after quiz starts", async () => {
+        var T = 10;
+        while (T--) {
+            await quiz.get_current_block();
+        }
+        let res1 = await quiz.get_question({from: accounts[1]});
+    })
+
+    it("tests that the player can submit an answer within question deadline", async () => {
+        let res1 = await quiz.submit_answer(1, 4, {from: accounts[1]});
+    })
+
+    it("tests that an unregistered player cannot submit an answer", async () => {
+        let res1 = quiz.submit_answer(1, 4, {from: accounts[3]});
+        await expectThrow(res1);
     })
 
 });
